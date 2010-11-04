bc02af24290a1316076ff270479d1ef92d854117
RobertLeePlummerJr <RobertLeePlummerJr@7636c594-314c-0410-bac7-8bf776cd2d1b>
2010-11-04 03:01:00 +0000

    [ADD] Added startup setting "forceColWidthsOnStartup".  If colgroup/col objects exist this will force sheet to grab the col widths from them.
    git-svn-id: http://jquerysheet.googlecode.com/svn/trunk@297 7636c594-314c-0410-bac7-8bf776cd2d1b

--- jquery.sheet.js
+++ jquery.sheet.js
@@ -65,7 +65,8 @@ jQuery.fn.extend({
 			caseInsensitive: 	false,							//bool, this makes all the calculations engine user functions case sensitive/insensitive
 			resizable: 			true,							//bool, makes the $(obj).sheet(); object resizeable, also adds a resizable formula textarea at top of sheet
 			autoFiller: 		false,							//bool, the little guy that hangs out to the bottom right of a selected cell, users can click and drag the value to other cells
-			minSize: 			{rows: 15, cols: 5}				//object - {rows: int, cols: int}, Makes the sheet stay at a certain size when loaded in edit mode, to make modification more productive
+			minSize: 			{rows: 15, cols: 5},			//object - {rows: int, cols: int}, Makes the sheet stay at a certain size when loaded in edit mode, to make modification more productive
+			forceColWidthsOnStartup:true						//bool, makes cell widths load from pre-made colgroup/col objects, use this if you plan on making the col items, makes widths more stable on startup
 		}, settings);
 		
 		o = settings.parent;
@@ -497,8 +498,7 @@ jQuery.sheet = {
 					var widthFn;
 					
 					if (reloadWidths) {
-						parents = o.find('tr:first td');
-						parents.add(o.find('tr:first th'));
+						parents = o.find('tr:first').find('td,th'));
 						widthFn = function(obj) {
 							return jS.attrH.width(obj);
 						};
@@ -2305,6 +2305,7 @@ jQuery.sheet = {
 			},
 			cellEdit: function(td, isDrag) { /* starts cell to be edited
 												td: object, td object;
+
 												isDrag: bool, should be determained by if the user is dragging their mouse around setting cells;
 												*/
 				jS.autoFillerNotGroup = true; //make autoFiller directional again.
@@ -3625,6 +3626,49 @@ jQuery.sheet = {
 								.replace(/\r/g,'');
 					}
 					return v;
+				},
+				pow1p:	function(x, y) {
+				    if (Math.abs(x) > 0.5) {
+				        return Math.pow(1 + x, y);
+				    } else {
+				        return Math.exp(y * Math.log(x));
+				    }
+				},
+				pow1pm1: function(x, y) {
+				    if (x <= -1) {
+				        return Math.pow(1 + x, y) - 1;
+				    } else {
+				        return cE.cFN.expm1(y * Math.log(x));
+				    }
+				},
+				expm1: function(x) {
+					var ret=0, n = 50; // degree of precision
+					var factorial = function factorial (n) {
+						if ((n === 0) || (n === 1)) {
+							return 1;
+						} else {
+							var result = (n * factorial(n-1) );
+							return result;
+						}
+				    };
+				    
+				    for (var i=1; i < n; i++) {
+				    	ret += Math.pow(x, i) / factorial(i);
+				    }
+				    
+				    return ret;
+				},
+				getPay: function(rate, nper, pv, fv, type) {
+				    var pvif;
+				    var fvifa;
+				    
+				    fv = (fv ? fv : 1);
+				    type = (type ? type : 1);
+				    
+				    pvif  = Math.pow( 1 + rate, nper );
+				    fvifa = ( pvif - 1 ) / rate;
+			
+				    return ( -pv * pvif - fv ) / ( ( 1.0 + rate * type ) * fvifa );
 				}
 			},
 			fn: {//fn = standard functions used in cells
@@ -3922,12 +3966,50 @@ jQuery.sheet = {
 				SQRT: function(v) {
 					return Math.sqrt(v);
 				},
-				PMT: function(interest_rate, number_payments, principal_value, FV) {
+				PMTT: function(interest_rate, number_payments, principal_value, FV) {
 					FV = (FV ? FV : 1000);
-					return -1 * this.ROUND(1 + interest_rate * (FV - Math.pow(1 + interest_rate, number_payments) * principal_value) / -(-1 + Math.pow((1 + interest_rate), number_payments)), 2);
+					return interest_rate * (FV - Math.pow(1 + interest_rate, number_payments) * principal_value) / -(-1 + Math.pow((1 + interest_rate), number_payments));
 				},
-				NPER: function(rate, pmt, pv, fv) {
-					return Math.log((fv + pmt/rate) / (pv + pmt/rate)) / Math.log(1+rate);
+				PMT: function(rate, nper, pv, fv){
+					var pmt_value = 0;
+					rate = rate / 100;
+					fv = parseFloat(fv ? fv : 0); //optional
+
+					if ( rate == 0 ) {
+						pmt_value = - (fv + pv)/nper;	
+					} else {
+						x = Math.pow(1 + rate,nper);
+						pmt_value = -((rate * (fv + x * pv))/(-1 + x));
+					}
+					
+					return this.ROUND(pmt_value, 2);
+				},
+				NPER: function(rate, nper, pv, fv){ //STill doesn't work right
+					//fv = parseFloat(fv ? fv : 1);
+					var nper = cE.cFN.getPay(rate, nper, pv, fv); 
+					return nper;
+				},
+				FVV: function(rate, nper, pmt, pv){
+					var fv_value = 0;
+					pv = (pv ? pv : 1);
+					
+					if ( rate == 0 ) {
+						fv_value = -(pv + (pmt * nper));
+					} else {
+						x = Math.pow(1 + rate, nper);
+						fv_value = -( -pmt + x * pmt + rate * x * pv ) /rate;
+					}
+					fv_value = this.ROUND(fv_value, 2);
+					return (fv_value);
+				},
+				FV: function(rate, nper, pmt, pv, type) {
+					type = (type ? type : 0);
+
+					pvif  = calculate_pvif (rate, nper);
+					fvifa = calculate_fvifa (rate, nper);
+
+				        return (-((pv * pvif) + pmt *
+								  (1.0 + rate * type) * fvifa));
 				},
 				CELLREF: function(v, i) {
 					var td;
@@ -4383,7 +4465,7 @@ jQuery.sheet = {
 			}
 		}
 		
-		jS.openSheet(o);
+		jS.openSheet(o, s.forceColWidthsOnStartup);
 		
 		return jS;
 	},
