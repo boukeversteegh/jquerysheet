05e17e5e25214cdd989b796b2b01a416c57d8b1d
robertleeplummerjr <robertleeplummerjr@7636c594-314c-0410-bac7-8bf776cd2d1b>
2012-11-26 20:26:49 +0000

    [FIX] a few problems with return value, enabled and visible checking, use 'val' rather than 'value' as it was providing direct access to the object's value
    git-svn-id: http://jquerysheet.googlecode.com/svn/trunk@501 7636c594-314c-0410-bac7-8bf776cd2d1b

--- jquery.pseudosheet.js
+++ jquery.pseudosheet.js
@@ -41,12 +41,20 @@ jQuery.pseudoSheet = { //jQuery.pseudoSheet
 					isInput = $obj.is(':input');
 
 				if (isInput) {
-					obj.value = $obj.val();
+					if ($obj.is(':radio,:checkbox')) {
+						if ($obj.is(':checked')) {
+							obj.val = $obj.val();
+						} else {
+							obj.val = '';
+						}
+					} else {
+						obj.val = $obj.val();
+					}
 				} else {
-					obj.value = $obj.html();
+					obj.val = $obj.html();
 				}
 
-				$obj.data('oldValue', obj.value); //we detect the last value, so that we don't have to update all objects, thus saving resources
+				$obj.data('oldValue', obj.val); //we detect the last value, so that we don't have to update all objects, thus saving resources
 
 				if ($obj.data('state')) {
 					throw("Error: Loop Detected");
@@ -117,36 +125,36 @@ jQuery.pseudoSheet = { //jQuery.pseudoSheet
 							obj.result = Parser.parse(obj.formula);
 						} catch(e) {
 							console.log(e);
-							obj.value = e.toString().replace(/\n/g, '<br />'); //error
+							obj.val = e.toString().replace(/\n/g, '<br />'); //error
 						}
 						jP.callStack--;
-					}
 
-					if (obj.result) {
-						if (obj.result.value) {
-							obj.value = obj.result.value;
+						if (typeof obj.result != 'undefined') {
+							if (obj.result.value) {
+								obj.val = obj.result.value;
+							} else {
+								obj.val = obj.result;
+							}
+							if (!obj.result.html && !obj.result.value) {
+								obj.result = {val: obj.result, html: obj.result};
+							} else {
+								obj.result.html = obj.val;
+							}
 						} else {
-							obj.value = obj.result;
+							obj.result = {html: obj.val};
 						}
-						if (!obj.result.html && !obj.result.value) {
-							obj.result = {value: obj.result, html: obj.result};
-						} else {
-							obj.result.html = obj.value;
-						}
-					} else {
-						obj.result = {html: obj.value};
-					}
 
-					if (isInput) {
-						$obj.val(obj.value);
-					} else {
-						$obj.html(obj.result.html);
+						if (isInput) {
+							$obj.val(obj.val);
+						} else {
+							$obj.html(obj.result.html);
+						}
 					}
 				}
 
-				$obj.data('state', null);
+				$obj.removeData('state');
 
-				return obj.value;
+				return obj.val;
 			},
 			objHandler: {
 				callFunction: function(fn, args, obj) {
@@ -196,8 +204,8 @@ jQuery.pseudoSheet = { //jQuery.pseudoSheet
 
 					if (varName.length > 1) {
 						switch (varName[1]) {
-							case "visible": return ($obj.is(':visible') ? 'TRUE' : 'FALSE');
-							case "enabled": return ($obj.is(':enabled') ? 'TRUE' : 'FALSE');
+							case "visible": return ($obj.is(':visible') ? true : false);
+							case "enabled": return ($obj.is(':enabled') ? true : false);
 							case "value":   return jP.objHandler.getObjectValue($obj);
 							default:        throw("Error: Attribute not found");
 						}
