655aa753f030ae959d5164114d31ddc50776ea48
robertleeplummerjr <robertleeplummerjr@7636c594-314c-0410-bac7-8bf776cd2d1b>
2012-12-05 22:36:29 +0000

    [FIX] Corrected many of the errors with financialfn, added rate and impt, and created unit tests for them [ENH] Split the value and html to use globalize to display values
    git-svn-id: http://jquerysheet.googlecode.com/svn/trunk@515 7636c594-314c-0410-bac7-8bf776cd2d1b

--- jquery.sheet.financefn.js
+++ jquery.sheet.financefn.js
@@ -1,4 +1,4 @@
-jQuery.sheet.financefn = {
+var jSF = jQuery.sheet.financefn = {
 	NPV: function(rate) {
 		rate = rate * 1;
 		var factor = 1;
@@ -9,35 +9,119 @@ jQuery.sheet.financefn = {
 			sum += arguments[i] / factor;
 		}
 		
-		return sum;
+		return {
+			value: sum,
+			html: Globalize.format( sum, "c" )
+		};
+	},
+	PV: function(rate, nper, pmt, fv, type) {
+		fv = fv || 0;
+		type = type || 0;
+
+		var pv;
+		if (rate != 0) {
+			pv = (-pmt * (1 + rate * type) * ((Math.pow(1 + rate, nper) - 1) / rate) - fv) / Math.pow(1 + rate, nper);
+		} else {
+			pv = -fv - pmt * nper;
+		}
+
+		return {
+			value: pv,
+			html: Globalize.format( pv, "c" )
+		};
+	},
+	RATE: function(nper, pmt, pv, fv, type, estimate) {
+		fv = fv || 0;
+		type = type || 0;
+		estimate = estimate || 0.1;
+
+		var rate = estimate, y = 0, f = 0,
+			FINANCIAL_MAX_ITERATIONS = 128,
+			FINANCIAL_PRECISION = 1.0e-08;
+		if (Math.abs(rate) < FINANCIAL_PRECISION) {
+			y = pv * (1 + nper * rate) + pmt * (1 + rate * type) * nper + fv;
+		} else {
+			f = Math.exp(nper * Math.log(1 + rate));
+			y = pv * f + pmt * (1 / rate + type) * (f - 1) + fv;
+		}
+		var y0 = pv + pmt * nper + fv,
+			y1 = pv * f + pmt * (1 / rate + type) * (f - 1) + fv;
+
+		// find root by secant method
+		var i = 0, x0 = 0,
+			x1 = rate;
+		while ((Math.abs(y0 - y1) > FINANCIAL_PRECISION) && (i < FINANCIAL_MAX_ITERATIONS)) {
+			rate = (y1 * x0 - y0 * x1) / (y1 - y0);
+			x0 = x1;
+			x1 = rate;
+
+			if (Math.abs(rate) < FINANCIAL_PRECISION) {
+				y = pv * (1 + nper * rate) + pmt * (1 + rate * type) * nper + fv;
+			} else {
+				f = Math.exp(nper * Math.log(1 + rate));
+				y = pv * f + pmt * (1 / rate + type) * (f - 1) + fv;
+			}
+
+			y0 = y1;
+			y1 = y;
+			++i;
+		}
+
+		return {
+			value: rate,
+			html: Globalize.format( rate, "p" )
+		};
+	},
+	IPMT: function(rate, per, nper, pv, fv, type) {
+		var pmt = jFN.PMT(rate, nper, pv, fv, type).value,
+			fv = jFN.FV(rate, per - 1, pmt, pv, type).value,
+			result = fv * rate;
+
+		// account for payments at beginning of period versus end.
+		if (type) {
+			result /= (1 + rate);
+		}
+
+		// return results to caller.
+		return {
+			value: result,
+			html: Globalize.format( result, "c" )
+		};
 	},
 	PMT: function(rate, nper, pv, fv, type){
-		fv = (fv ? fv : 0);
-		type = (type ? type : 0);
-		var invert = (pv < 0 ? true : false);
-		pv = Math.abs(pv);
-		
-		var v = ((-rate * (pv * Math.pow(1.0 + rate, nper) + fv)) /
-				((1.0 + rate * type) * (Math.pow(1.0 + rate, nper) - 1))
-			);
-		
-		return (invert ? -v : v);
-	},
-	NPER: function(rate, payment, pv, fv, type) { //not working yet
-		fv = (fv ? fv : 0);
-		type = (type ? type : 0);
-		var invert = (payment < 0 ? true : false);
-		payment = Math.abs(payment);
-		
-		var v = (
-			Math.log(
-				(-payment * (1.0 + rate * type) + (-1.0 / rate) * fv) /
-				(pv * rate + -payment * (1.0 + rate * type))
-			) /
-			Math.log(1.0 + rate)
-		);
+		fv = fv || 0;
+		type = type || 0;
 
-		return (invert ? v : -v);
+		// pmt = rate / ((1 + rate)^N - 1) * -(pv * (1 + r)^N + fv)
+		var pmt = rate / (Math.pow(1 + rate, nper) - 1)
+			* -(pv * Math.pow(1 + rate, nper) + fv);
+
+		// account for payments at beginning of period versus end.
+		if (type == 1) {
+			pmt = pmt / (1 + rate);
+		}
+
+		// return results to caller.
+		return {
+			value: pmt,
+			html: Globalize.format( pmt, "c" )
+		};
+	},
+	NPER: function(rate, pmt, pv, fv, type) { //not working yet
+		type = type || 0;
+		if ((rate == 0) && (pmt != 0)) {
+			var nper = (-(fv + pv) / pmt);
+		} else if (rate <= 0.0) {
+			return null;
+		} else {
+			var tmp = (pmt * (1.0 + rate * type) - fv * rate) /
+				(pv * rate + pmt * (1.0 + rate * type));
+			if (tmp <= 0.0) {
+				return null;
+			}
+			var nper = (math.log10(tmp) / math.log10(1.0 + rate));
+		}
+		return (isFinite(nper) ? nper: null);
 	},
 	FV: function(rate, nper, pmt, pv, type) { //not working yet
 		pv = (pv ? pv : 0);
