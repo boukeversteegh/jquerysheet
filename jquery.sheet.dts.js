381cd8fd0a493c6518a7ee039b2dcc81c6a02065
robertleeplummerjr <robertleeplummerjr@7636c594-314c-0410-bac7-8bf776cd2d1b>
2013-01-03 18:04:52 +0000

    [FIX] Fixes to xml document structure
    git-svn-id: http://jquerysheet.googlecode.com/svn/trunk@556 7636c594-314c-0410-bac7-8bf776cd2d1b

--- jquery.sheet.dts.js
+++ jquery.sheet.dts.js
@@ -5,7 +5,61 @@
 			xml: function(xml) {
 				xml = $(xml);
 
+				var tables = $([]);
 
+				xml.each(function(spreadsheet) { //spreadsheets
+					$(this).children().each(function() {
+						var table = $('<table />').attr('title', $(this).attr('title') || ''),
+							colgroup = $('<colgroup/>').appendTo(table),
+							tbody = $('<tbody />').appendTo(table);
+
+						tables = tables.add(table);
+						$(this).children().each(function(row) {
+							switch (this.nodeName.toLowerCase()) {
+								case 'row':
+									var tr = $('<tr/>').appendTo(tbody);
+									tr
+										.css('height', $(this).attr('height'))
+										.attr('height', $(this).attr('height'));
+
+									$(this).children().each(function() { //column
+										//console.log(this.nodeName.toLowerCase());
+										var td = $('<td />').appendTo(tr);
+										$(this).children().each(function() { //formula or value or style
+											switch (this.nodeName.toLowerCase()) {
+												case 'formula':
+													td.attr('formula', $(this).html());
+													break
+												case 'value':
+													td.html($(this).html());
+													break;
+												case 'style':
+													td.attr('style', $(this).html());
+													break;
+												case 'class':
+													td.attr('class', $(this).html());
+											}
+										});
+									});
+									break;
+								case 'metadata':
+									$(this).children().each(function() {
+										switch (this.nodeName.toLowerCase()) {
+											case 'width':
+												$('<col/>')
+													.attr('width', $(this).text())
+													.css('width', $(this).text())
+													.appendTo(colgroup);
+												break;
+										}
+									});
+									break;
+							}
+						});
+					});
+				});
+
+				return tables;
 			},
 			size: function() {}
 		},
@@ -13,12 +67,12 @@
 			json: function(jS) {
 			},
 			xml: function(jS) {
-				var output = '<spreadsheets>';
+				var output = '<spreadsheets title="' + (jS.s.title || '') + '">';
 
 				$.each(jS.spreadsheets, function(sheet) {
 					jS.i = sheet;
 					var metadata = [];
-					output += '<spreadsheet>';
+					output += '<spreadsheet title="' + (jS.obj.sheet().attr('title') || '') + '">';
 
 					$.each(jS.spreadsheets[sheet], function (row) {
 						if (row == 0) return;
@@ -34,16 +88,14 @@
 							if (this.td.attr('style')) output += '<style>' + this.td.attr('style') + '</style>'
 							output += '</column>';
 
-							if (row == 1) {
+							if (row * 1 == 1) {
 								metadata[column] = '<width>' + $(jS.col(null, column)).css('width') + '</width>'
 							}
 						});
 						output += '</row>';
 					});
 
-					output += '<metadata>' +
-						'<title>' + jS.s.title + '</title>'
-					metadata.join('') + '</metadata>';
+					output += '<metadata>' + metadata.join('') + '</metadata>';
 
 					output += '</spreadsheet>';
 				});
